70e2b289f1668c0c4888af96f6d6b0a07bb13d09
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-04-12 13:42:23 +0800

    Add files via upload

--- RunPy.bat
+++ RunPy.bat
@@ -2,6 +2,13 @@
 :: 将控制台代码页更改为 UTF-8
 chcp 65001 > nul
 :: 运行程序
-start "LunaHook" C:\Users\11248\miniconda3\envs\LunaHook_log\python.exe %1\main.py
+if "%~1"=="" (
+    echo %1 是空的
+	start "LunaHook" C:\Users\11248\miniconda3\envs\LunaHook_log\python.exe %CD%\main.py
+) else (
+    echo %1 不是空的
+	cd /d %1
+	start "LunaHook" C:\Users\11248\miniconda3\envs\LunaHook_log\python.exe %1\main.py
+)
 :: 等待用户按任意键继续
 pause
